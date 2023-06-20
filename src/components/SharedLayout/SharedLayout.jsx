f69b4f9ff56d30af80016018da6f2ac59c1b36ad
Yurii-Bodnar <yuriy.bodnar75@gmail.com>
2023-06-20 14:37:53 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,11 +2,10 @@ import Header from 'components/Header/Header';
 import Loader from 'components/Loader/Loader';
 import { Suspense } from 'react';
 import { useSelector } from 'react-redux';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { isSideBarOpen } from 'redux/auth/authSelectors';
 
 const SharedLayout = () => {
-  const location = useLocation();
   const isModalOpen = useSelector(isSideBarOpen);
   return (
     <Suspense fallback={<Loader />}>
